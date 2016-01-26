f51eef4bedd8719573a646b208abae6e238d01b0
Bastien <basgys@gmail.com>
2016-01-26 22:29:16 +0100

    Initial commit

---
+++ decoder.go
@@ -0,0 +1,71 @@
+package xml2json
+
+import (
+	"encoding/xml"
+	"io"
+)
+
+// A Decoder reads and decodes XML objects from an input stream.
+type Decoder struct {
+	r   io.Reader
+	err error
+}
+
+type element struct {
+	parent *element
+	n      *Node
+	label  string
+}
+
+// NewDecoder returns a new decoder that reads from r.
+func NewDecoder(r io.Reader) *Decoder {
+	return &Decoder{r: r}
+}
+
+// Decode reads the next JSON-encoded value from its
+// input and stores it in the value pointed to by v.
+func (dec *Decoder) Decode(root *Node) error {
+	xmlDec := xml.NewDecoder(dec.r)
+
+	// Create first element from the root node
+	elem := &element{
+		parent: nil,
+		n:      root,
+	}
+
+	for {
+		t, _ := xmlDec.Token()
+		if t == nil {
+			break
+		}
+
+		switch se := t.(type) {
+		case xml.StartElement:
+			// Build new a new current element and link it to its parent
+			elem = &element{
+				parent: elem,
+				n:      &Node{},
+				label:  se.Name.Local,
+			}
+
+			// Extract attributes as children
+			for _, a := range se.Attr {
+				// TODO : Prefix attribute to avoid clashes
+				elem.n.AddChild(a.Name.Local, &Node{Data: a.Value})
+			}
+		case xml.CharData:
+			// Extract XML data (if any)
+			elem.n.Data = string(xml.CharData(se))
+		case xml.EndElement:
+			// And add it to its parent list
+			if elem.parent != nil {
+				elem.parent.n.AddChild(elem.label, elem.n)
+			}
+
+			// Then change the current element to its parent
+			elem = elem.parent
+		}
+	}
+
+	return nil
+}
---
+++ encoder.go
@@ -0,0 +1,76 @@
+package xml2json
+
+import "io"
+
+// An Encoder writes JSON objects to an output stream.
+type Encoder struct {
+	w   io.Writer
+	err error
+}
+
+// NewEncoder returns a new encoder that writes to w.
+func NewEncoder(w io.Writer) *Encoder {
+	return &Encoder{w: w}
+}
+
+// Encode writes the JSON encoding of v to the stream
+func (enc *Encoder) Encode(root *Node) error {
+	if enc.err != nil {
+		return enc.err
+	}
+	if root == nil {
+		return nil
+	}
+
+	enc.err = enc.format(root, 0)
+
+	return enc.err
+}
+
+func (enc *Encoder) format(n *Node, lvl int) error {
+	if n.IsComplex() {
+		enc.write("{")
+
+		i := 0
+		tot := len(n.Children)
+		for label, children := range n.Children {
+			enc.write("\"")
+			enc.write(label)
+			enc.write("\": ")
+
+			if len(children) > 1 {
+				// Array
+				enc.write("[")
+				for j, c := range children {
+					enc.format(c, lvl+1)
+
+					if j < len(children)-1 {
+						enc.write(", ")
+					}
+				}
+				enc.write("]")
+			} else {
+				// Map
+				enc.format(children[0], lvl+1)
+			}
+
+			if i < tot-1 {
+				enc.write(", ")
+			}
+			i++
+		}
+
+		enc.write("}")
+	} else {
+		// TODO : Extract data type
+		enc.write("\"")
+		enc.write(n.Data)
+		enc.write("\"")
+	}
+
+	return nil
+}
+
+func (enc *Encoder) write(s string) {
+	enc.w.Write([]byte(s))
+}
